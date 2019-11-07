841b1e731624a1fd3971112a9e6c131a7c0074aa
milankl <milankloewer@gmx.de>
2019-11-07 14:47:32 +0000

    matrix solve example

--- src/Sherlogs.jl
+++ src/Sherlogs.jl
@@ -7,12 +7,6 @@ import Base: (+), (-), (*), (/)
 const n16 = 2^16                        # number of bitpatterns for a 16bit number format
 const logbook = zeros(UInt64,n16)       # bitpattern histogram array
 
-const idx = [1]                         # index for stacktrace array, kept in array for mutability
-const idxmax = 1001
-const stmax = 5
-# stacktrace array to store stacktraces
-const SA = Array{Base.StackTraces.StackFrame,2}(undef,stmax,idxmax-1)
-
 include("Sherlog64.jl")
 include("Sherlog32.jl")
 include("Sherlog16.jl")
--- src/logbook.jl
+++ src/logbook.jl
@@ -12,11 +12,5 @@ end
 
 """Count arithmetic result in logbook."""
 function logit(::Type{T},x::AbstractFloat) where {T<:AbstractFloat}
-
-    if x > 1000.0
-        SA[:,idx[1]] = stacktrace()[1:stmax]
-        idx[1] = mod(idx[1]+1,idxmax)
-    end
-
     logbook[reinterpret(UInt16,T(x)) + 1] += 1
 end
