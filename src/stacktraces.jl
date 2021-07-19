6708fe52d6cce88d74954ad3070ca8eb39ea7334
Milan <milankloewer@gmx.de>
2021-07-19 18:51:49 +0100

    get_stacktrace, bump to v0.2

--- src/stacktraces.jl
+++ src/stacktraces.jl
@@ -1,4 +1,4 @@
-function scent(f,r::Real)
+function scent(f::Function,r::Real)
     if f(r)
         i = st_index[1]
         if i <= nstackframes
@@ -13,8 +13,8 @@ function scent(f,r::Real)
     end
 end
 
-const empty_sym = Symbol("")
-const unknown_st = Base.StackTraces.StackFrame(empty_sym,empty_sym,-1,nothing,true,false,0)
+empty_symbol = Symbol("")
+const unknown_st = Base.StackTraces.StackFrame(empty_symbol,empty_symbol,-1,nothing,true,false,0)
 
 const nstackframes = 10000      # quite arbitrary for the moment
 const stackframe_depth = 3
@@ -22,5 +22,6 @@ const stackframe_depth = 3
 const ST = [unknown_st for _ in 1:stackframe_depth, _ in 1:nstackframes]
 const st_index = [1]
 
+get_stacktrace(i::Int=1) = ST[:,i]
 get_stacktraces() = ST[:,1:st_index[1]-1]
 get_st_index() = st_index[1]
